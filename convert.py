ee5c760d095c6f2037d26f450c6adb5d2e1200d0
Your Name <you@example.com>
2016-02-17 13:59:10 -0500

    sample levels

--- convert.py
+++ convert.py
@@ -1,5 +1,12 @@
 import cc_dat_utils
+import cc_json_utils
+import json
 
-from_dat = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
+
+
+json_reader = open('data\jcossins_ccl.json', "r")
+json_data = json.load(json_reader)
+json_reader.close() #Close the file now that we're done using it
+from_dat = cc_json_utils.make_cc_data_from_json(json_data)
 
 print(from_dat)
